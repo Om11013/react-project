19b8491bae5af0e5c26de557cf12f644cad400da
Om11013 <poonjaniom11@gmail.com>
2026-04-04 15:34:53 +0530

    [chore] convert ButtonProps to type alias and remove redundant comment in Button component

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -2,15 +2,13 @@ import React from 'react';
 
 import { cn } from '../../utils/cn';
 
-export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
-
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, children, ...props }, ref) => {
     return (
       <button
         ref={ref}
         className={cn(
-          // Base common styles
           'inline-flex items-center justify-center transition-colors',
           'focus:outline-none',
           'disabled:opacity-50 disabled:pointer-events-none cursor-pointer',
